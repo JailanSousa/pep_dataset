8bbf0f2f9243eaa6a98ba5c205842b4769944f89
JailanSousa <jailann22silvasousa@gmail.com>
2024-03-14 20:43:47 -0300

    updating

--- remove_redundancy.py
+++ remove_redundancy.py
@@ -3,7 +3,7 @@ import os
 
 #os.chdir('/home/jssousa/pep_dataset/datasets')
 
-file = 'search_accession.csv'
+file = 'dataset.csv'
 
 df = pd.read_csv(file)
 
@@ -11,8 +11,8 @@ df = pd.read_csv(file)
 
 #df_non_redundante.to_excel('keep_first_non_redundant_pepset_uniprot_end_ncbi.xlsx', index=False)
 
-df_non_redd = df.drop_duplicates(subset=['Entry'], keep='first')
+df_non_redd = df.drop_duplicates(subset=['Sequence'], keep='first')
 
-df_non_redd.to_csv('search_accession.csv', index=False)
+df_non_redd.to_csv('dataset.csv', index=False)
 
 print('OK')
